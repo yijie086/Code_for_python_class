262876b349ba786ad714d0f2bb41433f9dfaae8c
Yijie Wang <70418431+ustcyjwang@users.noreply.github.com>
2022-10-17 15:46:33 -0500

    Update hw7_3.py

--- hw7/hw7_3.py
+++ hw7/hw7_3.py
@@ -1,4 +1,4 @@
-print("This is a python code for homework7-2: Read and Write")
+print("This is a python code for homework7-3: Read and Write")
 f = open("hw7_3_READ_DAT.txt", encoding="utf-8")
 temp = f.read()
 f.close()
